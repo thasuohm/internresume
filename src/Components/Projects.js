b204f9695e05d2092ba612709357002f1146e951
thasuohm <thasuohm@gmail.com>
2021-11-15 21:47:05 +0700

    add calculator project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,14 +1,15 @@
 import React from "react";
-import project11 from "../images/project1-1.png";
-import project12 from "../images/project1-2.png";
-import project13 from "../images/project1-3.png";
-import project21 from "../images/project2-1.png";
-import project22 from "../images/project2-2.png";
-import project31 from "../images/welcomepage.mp4";
-import project32 from "../images/logindemo.mp4";
-import project33 from "../images/filterandsearch.mp4";
-import project34 from "../images/addtocartloginandseeorder.mp4";
-import project35 from "../images/comment.mp4";
+import project11 from "../images/projects/project1-1.png";
+import project12 from "../images/projects/project1-2.png";
+import project13 from "../images/projects/project1-3.png";
+import project21 from "../images/projects/project2-1.png";
+import project22 from "../images/projects/project2-2.png";
+import project31 from "../images/projects/welcomepage.mp4";
+import project32 from "../images/projects/logindemo.mp4";
+import project33 from "../images/projects/filterandsearch.mp4";
+import project34 from "../images/projects/addtocartloginandseeorder.mp4";
+import project35 from "../images/projects/comment.mp4";
+import calculatorApp from "../images/projects/calculatorApp.mp4";
 import "../css/projects.css";
 
 function Projects() {
@@ -31,8 +32,8 @@ function Projects() {
       title: "#3 Online Shopping Application",
       description:
         ", This project is a project that has authentication users to access and manage information for their own users only. we have three roles of users in this system, to practice managing users in various roles and use redux to handle state management.",
-      img: [project31, project32, project33, project34, project35],
-      imgDes: [
+      vid: [project31, project32, project33, project34, project35],
+      vidDes: [
         "demo welcome page",
         "demo login",
         "demo search and filter",
@@ -44,6 +45,13 @@ function Projects() {
         url: "https://www.phoenixnext.com/new-release.html",
         title: "phoenixnext",
       },
+    },
+    {
+      title: "#4 Calculator Application",
+      description:
+        ", This project was created to practice javascript and react skills.",
+      vid: [calculatorApp],
+      builtWith: "React",
     },
   ];
 
@@ -65,7 +73,7 @@ function Projects() {
               <div className="mt-20 b noto">Built with : {pro.builtWith}</div>
             </div>
 
-            {i <= 1 &&
+            {pro.img &&
               pro.img.map((img, i) => {
                 return (
                   <img
@@ -77,8 +85,8 @@ function Projects() {
                 );
               })}
 
-            {i > 1 &&
-              pro.img.map((img, i) => {
+            {pro.vid &&
+              pro.vid.map((vid, i) => {
                 return (
                   <React.Fragment key={`videoNo${i}`}>
                     <video
@@ -86,12 +94,12 @@ function Projects() {
                       autoPlay
                       loop
                       muted
-                      src={img}
+                      src={vid}
                       type="video/mp4"
                     />
-                    {pro.imgDes && (
+                    {pro.vidDes && (
                       <div className="text-center pb-10 f18">
-                        {pro.imgDes[i]}
+                        {pro.vidDes[i]}
                       </div>
                     )}
                   </React.Fragment>
